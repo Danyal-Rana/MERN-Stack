a66d81f255721fd19adc8c35f9a3eec7a67fe89b
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-04-02 02:47:16 +0500

    github

---
+++ React/07reactRouter/src/components/github/Github.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Github = () => {
+    return (
+        <div className='bg-gray-700 m-4 text-center text-white p-4 text-3xl'>
+            Github Repositries: 
+            
+        </div>
+    );
+}
+
+export default Github;
