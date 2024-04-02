86d9ba597e86c9dfe270455ca9a953d10bfc8819
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-04-03 02:56:14 +0500

    done with context

--- React/08miniContext/src/App.jsx
+++ React/08miniContext/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import Profile from './Components/Profile'
+import Login from './Components/Login'
 import UserContextProvider from './context/UserContextProvider'
+
 
 function App() {
   
   return (
     <UserContextProvider>
       <h1>React with Chai and Share important hai</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   )
 }
