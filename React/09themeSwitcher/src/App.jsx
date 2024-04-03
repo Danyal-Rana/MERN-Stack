60929ff8712c3ff9354a7acdfa98b881e8ebd6a0
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-04-04 01:48:15 +0500

    provider

--- React/09themeSwitcher/src/App.jsx
+++ React/09themeSwitcher/src/App.jsx
@@ -2,24 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { themeProvider } from './contexts/theme'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* themeBtn */}
-        </div>
+    < themeProvider >
 
-        <div className="w-full max-w-sm mx-auto">
-          {/* Card */}
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            
+            {/* themeBtn */}
+          </div>
 
+          <div className="w-full max-w-sm mx-auto">
+            {/* Card */}
+
+          </div>
         </div>
       </div>
-    </div>
+
+    </ themeProvider >
 
   )
 }
