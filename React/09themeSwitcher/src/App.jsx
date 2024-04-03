1f71688e2e2463fe00817effaea05988abda347d
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-04-04 02:59:51 +0500

    context

--- React/09themeSwitcher/src/App.jsx
+++ React/09themeSwitcher/src/App.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { themeProvider } from './contexts/theme'
+import {darkTheme, lightTheme, themeMode} from './contexts/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
 
   return (
 
-    < themeProvider >
+    < themeProvider value={{themeMode, lightTheme, darkTheme}} >
 
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            
+
             {/* themeBtn */}
           </div>
 
