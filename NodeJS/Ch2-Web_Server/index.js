d27e40eba088929a6b511ed8583c6baf25e9406b
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-05-22 23:46:44 +0500

    card

--- NodeJS/Ch2-Web_Server/index.js
+++ NodeJS/Ch2-Web_Server/index.js
@@ -1,15 +1,23 @@
 const http = require('http');
+const fs = require('fs');
+
+// const index = fs.readFileSync('index.html', 'utf-8');
+const index = fs.readFileSync('data.json', 'utf-8');
 
 const data = {age:20};
 
 const server = http.createServer((req, res) => {
     console.log("Server Started...");
+    console.log (req.url);
 
     res.setHeader("dummyName", "dummyValue");
     res.setHeader('content-type', "application/json");
+    // res.setHeader('content-type', 'text/html')
 
     // res.end("<h1>Hello Web Socket Programming !</h1>");
-    res.end(JSON.stringify(data));
+    // res.end(JSON.stringify(data));
+
+    res.end(index);
 });
 
 server.listen(8080);
