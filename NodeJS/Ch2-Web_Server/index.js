04ccae899e2ca77f7bd645ce6a98f4cb882ec1ff
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-05-23 00:49:39 +0500

    settingHeaders

--- NodeJS/Ch2-Web_Server/index.js
+++ NodeJS/Ch2-Web_Server/index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 
-// const index = fs.readFileSync('index.html', 'utf-8');
-const index = fs.readFileSync('data.json', 'utf-8');
+const index = fs.readFileSync('index.html', 'utf-8');
+// const index = fs.readFileSync('data.json', 'utf-8');
 
 const data = {age:20};
 
@@ -11,8 +11,8 @@ const server = http.createServer((req, res) => {
     console.log (req.url);
 
     res.setHeader("dummyName", "dummyValue");
-    res.setHeader('content-type', "application/json");
-    // res.setHeader('content-type', 'text/html')
+    // res.setHeader('content-type', "application/json");
+    res.setHeader('content-type', 'text/html')
 
     // res.end("<h1>Hello Web Socket Programming !</h1>");
     // res.end(JSON.stringify(data));
