ac4c27ade3b76db083bad3a10ae7ae6bdc58fc99
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-05-24 18:25:44 +0500

    postmanAPI

--- ExpressJS/Ch3-ExpessJS/index.js
+++ ExpressJS/Ch3-ExpessJS/index.js
@@ -8,8 +8,28 @@ const express = require('express');
 const server = express(); // server has been started
 
 // here goes the code
-server.get('/', (req, res)=> {
-    res.send("Hello Express !");
+server.get('/demo', (req, res)=> {
+    res.sendStatus(201).send("Hello Express !");
+    // res.sendFile('index.html', { root: 'R:/MERN-Stack/ExpressJS/Ch3-ExpessJS' });
+    // res.json(products);
+    // res.sendStatus(200);
+})
+
+// there name: API - Endpoint- Route
+server.get('/', (req, res) => {
+    res.json({type:'GET'});
+})
+server.post('/', (req, res) => {
+    res.json({type:'POST'});
+})
+server.put('/', (req, res) => {
+    res.json({type:'PUT'});
+})
+server.delete('/', (req, res) => {
+    res.json({type:'DELETE'});
+})
+server.patch('/', (req, res) => {
+    res.json({type:'PATCH'});
 })
 
 
