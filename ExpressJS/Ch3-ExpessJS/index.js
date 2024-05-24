2db2c7a4c75e132d38978a5e98bafae64d84276b
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-05-24 17:54:44 +0500

    Hello Express

--- ExpressJS/Ch3-ExpessJS/index.js
+++ ExpressJS/Ch3-ExpessJS/index.js
@@ -1,5 +1,18 @@
 const fs = require('fs');
-
 const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readlinkSync('data.json', 'utf-8'));
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 const products = data.products;
+
+const express = require('express');
+
+const server = express(); // server has been started
+
+// here goes the code
+server.get('/', (req, res)=> {
+    res.send("Hello Express !");
+})
+
+
+server.listen(8080, () => { // server end
+    console.log ("Server Started...");
+});
