0e9d055033654da98700205279efdd7008b2ddaf
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-07-01 17:39:53 +0500

    cloudinary

---
+++ Chai aur Backend/Project/src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+// this configuration allows to upload files to cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_API_KEY,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if (! localFilePath) {
+            return null;
+        }
+
+        // uploading the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        })
+
+        // file has been uploaded on cloudinary
+        console.log("File has been uploaded successfully on Cloudinary.", response.url);
+        return response;
+
+    } catch {
+        fs.unlinkSync(localFilePath); // it will remove the locally save temp file as the upload operation got failed
+        return null;
+    }
+};
+
+export default uploadOnCloudinary;
