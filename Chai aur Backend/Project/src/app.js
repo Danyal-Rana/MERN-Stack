32f984695647905dee34c44395c6a0d84eab58d6
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-07-02 13:35:14 +0500

    all setUp configured

--- Chai aur Backend/Project/src/app.js
+++ Chai aur Backend/Project/src/app.js
@@ -17,9 +17,14 @@ app.use(express.json({limit: "16kb"})); // it means max 16kb data of json can be
 
 // app.use(express.urlencoded());
 app.use(express.urlencoded({extended: true})); // extended means that the object can be of any type (true) or just a string (false), objects can be nested
-
 app.use(express.static("public")); // to serve/store static files like pdf images etc
-
 app.use(cookieParser()); // to access and set the cookies of client
+
+
+// routes portion
+import userRouter from "./routes/user.routes.js"; // alias can only be used when the file is exported as default
+
+// routes declaration
+app.use("/api/v1/users", userRouter); // https://localhost:8000/api/v1/users/register
 
 export default app;
--- Chai aur Backend/Project/src/db/index.js
+++ Chai aur Backend/Project/src/db/index.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         // console.log(connectionInstance);
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+        console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MONGODB connection Failed: ", error);
         process.exit(1);
