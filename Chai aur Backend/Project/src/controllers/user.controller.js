2c530279a2aedcba964d785cb6b9c289a872e7ab
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-07-04 13:21:29 +0500

    feat: Update account details endpoint

--- Chai aur Backend/Project/src/controllers/user.controller.js
+++ Chai aur Backend/Project/src/controllers/user.controller.js
@@ -236,7 +236,28 @@ const getCurrentUser = asyncHandler( async (req, res) => {
     .json(200, req.user, "User fetched successfully.");
 });
 
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const {fullName, username, email, password} = req.body;
 
+    if (!(fullName || email)) {
+        throw new ApiError(400, "All fields are required.");
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email: email,
+            }
+        },
+        {new: true}
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json (new ApiResponse(200, user, "Account details Updated Successfully."));
+})
 
 export {
     registerUser,
@@ -245,4 +266,5 @@ export {
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
+    updateAccountDetails
 }
