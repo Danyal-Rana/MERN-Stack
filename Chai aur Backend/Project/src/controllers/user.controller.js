7ad0b7e6cbc75d1603dfea81a3fdce6f7f7316cc
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-07-04 11:09:21 +0500

    feat: Add getCurrentUser endpoint

--- Chai aur Backend/Project/src/controllers/user.controller.js
+++ Chai aur Backend/Project/src/controllers/user.controller.js
@@ -230,6 +230,12 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully."));
 });
 
+const getCurrentUser = asyncHandler( async (req, res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "User fetched successfully.");
+});
+
 
 
 export {
@@ -237,4 +243,6 @@ export {
     loginUser,
     logoutUser,
     refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
 }
